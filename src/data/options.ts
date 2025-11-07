217d42dff9ce7e2740ac1a8a76a31c73b372e56d
Melanie Tong <melaniietong@gmail.com>
2025-11-07 15:47:35 -0500

    format labels for display

--- src/data/options.ts
+++ src/data/options.ts
@@ -1,22 +1,23 @@
 import type { DropdownOption } from '@/types/Dropdown'
 import { LANGUAGE, CHARACTER, ROMANIZATION } from '@/constants/constants'
+import { formatLabel } from '@/utils/strings'
 
 export const characterOptions: DropdownOption[] = [
-    { value: CHARACTER.TRADITIONAL, label: CHARACTER.TRADITIONAL },
-    { value: CHARACTER.SIMPLIFIED, label: CHARACTER.SIMPLIFIED }
+    { value: CHARACTER.TRADITIONAL, label: formatLabel(CHARACTER.TRADITIONAL) },
+    { value: CHARACTER.SIMPLIFIED, label: formatLabel(CHARACTER.SIMPLIFIED) }
 ]
 
 export const languageOptions: DropdownOption[] = [
-    { value: LANGUAGE.MANDARIN, label: LANGUAGE.MANDARIN },
-    { value: LANGUAGE.CANTONESE, label: LANGUAGE.CANTONESE }
+    { value: LANGUAGE.MANDARIN, label: formatLabel(LANGUAGE.MANDARIN) },
+    { value: LANGUAGE.CANTONESE, label: formatLabel(LANGUAGE.CANTONESE) }
 ]
 
 export const romanizationCantoneseOptions: DropdownOption[] = [
-    { value: ROMANIZATION.JYUTPING, label: ROMANIZATION.JYUTPING },
-    { value: ROMANIZATION.YALE, label: ROMANIZATION.YALE }
+    { value: ROMANIZATION.JYUTPING, label: formatLabel(ROMANIZATION.JYUTPING) },
+    { value: ROMANIZATION.YALE, label: formatLabel(ROMANIZATION.YALE) }
 ]
 
 export const romanizationMandarinOptions: DropdownOption[] = [
-    { value: ROMANIZATION.PINYIN, label: ROMANIZATION.PINYIN },
-    { value: ROMANIZATION.ZHUYIN, label: ROMANIZATION.ZHUYIN }
+    { value: ROMANIZATION.PINYIN, label: formatLabel(ROMANIZATION.PINYIN) },
+    { value: ROMANIZATION.ZHUYIN, label: formatLabel(ROMANIZATION.ZHUYIN) }
 ]
