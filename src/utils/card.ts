4ffbe0c2d190f28de867db45387b011684686c63
Melanie Tong <melaniietong@gmail.com>
2025-11-14 20:58:14 -0500

    center text

--- src/utils/card.ts
+++ src/utils/card.ts
@@ -7,11 +7,11 @@ const { character, language, phonetic } = useOptions()
 export const cardHtml = (data: Person): string => `
     <div class='bg-neutral-200 dark:bg-neutral-800 w-[150px] h-[230px] p-2 rounded-md cursor-pointer flex flex-col items-center justify-center gap-2'>
         <div class='flex flex-col gap-1 items-center'>
-            <span class='text-xs text-neutral-950 dark:text-neutral-50'>
+            <span class='text-xs text-neutral-950 dark:text-neutral-50 text-center'>
                 ${data[DATA_KEY.LANGUAGE][language.value][DATA_KEY.PHONETIC][phonetic.value]}
             </span>
 
-            <span class='text-xl text-neutral-950 dark:text-neutral-50 font-semibold'>
+            <span class='text-xl text-neutral-950 dark:text-neutral-50 font-semibold text-center'>
                 ${data[DATA_KEY.LANGUAGE][language.value][DATA_KEY.CHARACTER][character.value]}
             </span>
         </div>
@@ -21,11 +21,11 @@ export const cardHtml = (data: Person): string => `
         </div>
 
         <div class='flex flex-col gap-1 items-center'>
-            <span class='text-base text-neutral-950 dark:text-neutral-50'>
+            <span class='text-base text-neutral-950 dark:text-neutral-50 text-center'>
                 ${data['eng']}
             </span>
 
-            <span class='text-xs text-neutral-950 dark:text-neutral-50'>
+            <span class='text-xs text-neutral-950 dark:text-neutral-50 text-center'>
                 ${data['engSubtitle']}
             </span>
         </div>
